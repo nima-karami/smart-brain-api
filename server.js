903a88795dc12119acaa17d54d56a21043ba0c27
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-07-22 11:01:33 -0400

    adding profile

--- server.js
+++ server.js
@@ -38,6 +38,37 @@ app.post('/signin', (req, res) => {
     
 });
 
+app.post('/register', (req, res) => {
+    database.users.push({
+            id: '125',
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            entries: 0,
+            joined: new Date()
+    });
+
+    res.send(database.users);
+});
+
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            res.json(user);
+        }
+            
+        })
+
+    if (!found) {
+        res.status(404).json('no such user');
+    }
+});
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
