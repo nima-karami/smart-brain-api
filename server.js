a50e74bf03bfaf45779c50d7355dd28919ac83ec
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-07-27 17:42:12 -0400

    adding database

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-  });
+});
 
 
 
