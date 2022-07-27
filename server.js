3e6c5f7c509d369cf209c41c5f53687ee1f6fb3a
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-07-27 19:11:18 -0400

    run deploy

--- server.js
+++ server.js
@@ -35,7 +35,40 @@ app.get('/' , (req, res) => {res.send('app is working')});
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+app.post('/register', (req, res) => {const {email, name, password} = req.body;
+
+if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission')
+}
+
+const hash = bcrypt.hashSync(password);
+console.log('step 1');
+db.transaction(trx => {
+    console.log('step 2');
+    trx.insert({
+        hash: hash,
+        email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+        console.log('step 3');
+        trx('users')
+        .returning('*')
+        .insert({
+        name: name,
+        email: loginEmail[0].email,
+        joined: new Date()
+        })
+        .then(user => {
+        res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+})
+
+ .catch(err => res.status(400).json('unable to register'))});
 
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
